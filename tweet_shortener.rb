2fd5cc56f813834bc4a65165ab3a2d844432075d
Amalie S <amaliesmidth@gmail.com>
2018-09-05 11:23:38 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,34 @@
-# Write your code here.
+def dictionary
+  dictionary = {
+    'hello' => 'hi',
+    'to' => '2',
+    'two' => '2',
+    'too' => '2',
+    'for' => '4',
+    'four' => '4',
+    'you' => 'u',
+    'at' => '@',
+    'and' => '&'
+  }
+end
+
+def word_substituter(tweet)
+  tweet_as_array = tweet.split(' ')
+  tweet_as_array.map do |word|
+    if word == dictionary[]
+end
+
+
+def bulk_tweet_shortener(tweet_array)
+  
+end
+
+
+def selective_tweet_shortener(tweet)
+  
+end
+
+
+def shortened_tweet_truncator(tweet)
+  
+end
